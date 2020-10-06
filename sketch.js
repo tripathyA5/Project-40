8a0aeeaba3c9cc7fdfcf60a8731e3607d059a710
tripathyA5 <69359952+tripathyA5@users.noreply.github.com>
2020-10-05 20:11:42 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score=0;
+var player2score=0;
+// create the variables for the score and displaying scores. and intialize with zero
+
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  text("Player1:"+player1score,100,100);
+  textSize(20);
+  text("Player2:"+player2score,100,200);
+  textSize(20);
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
+if(fruits.isTouching(player1)){
+  player1score=player1score+1;
+  fruitGroup.destroyEach();
+}
+if(fruits.isTouching(player2)){
+  player2score=player2score+1;
+  fruitGroup.destroyEach();
+}
+}
